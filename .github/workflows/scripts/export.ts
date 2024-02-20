4849e4947b3dd7a233ff8b950d466da9a06774ab
JJ Adonis <work@josejai.me>
2024-02-20 09:53:25 +0800

    feature(ops): updater

---
+++ .github/workflows/scripts/export.ts
@@ -0,0 +1,9 @@
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN
+});
+
+const uids = process.env.COLLECTION_UIDS.split(',');
+
+console.log(uids);
