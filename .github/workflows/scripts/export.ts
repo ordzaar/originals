7a185f9ff450d8fbf679fb3fcbe113793b9cbd24
JJ Adonis <work@josejai.me>
2024-02-20 13:52:38 +0800

    feature(ops): updater

--- .github/workflows/scripts/export.ts
+++ .github/workflows/scripts/export.ts
@@ -1,14 +1,30 @@
 import { Octokit } from "@octokit/rest";
-
+import axios from "axios";
 
 async function main() {
   const octokit = new Octokit({
-    auth: process.env.GITHUB_ACCESS_TOKEN
+    auth: process.env.GITHUB_ACCESS_TOKEN,
   });
 
-  const uids = process.env.COLLECTION_UIDS?.split(',') ?? [];
+  const uids = process.env.COLLECTION_UIDS?.split(",") ?? [];
 
-  console.log(uids);
+  await Promise.all(
+    uids.map(async (uid) => {
+      const contents = await axios.post(
+        process.env.ENDPOINT,
+        {
+          collectionUid: uid,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.ORDZAAR_BEARER_TOKEN}`,
+            "x-lambda-bot": process.env.ORDZAAR_SECRET_KEY,
+          },
+        },
+      );
+      console.log(contents.data.meta.name);
+    }),
+  );
 }
 
-main().then(() => console.log('Done!'));
+main().then(() => console.log("Done!"));
