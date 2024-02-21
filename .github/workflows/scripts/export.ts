25205ad4f344f7c5b9b38e6f7c78f16923da7358
JJ Adonis <work@josejai.me>
2024-02-21 17:33:33 +0800

    feature(ops): update

--- .github/workflows/scripts/export.ts
+++ .github/workflows/scripts/export.ts
@@ -13,50 +13,48 @@ async function main() {
   const pathKey = process.env?.ORDZAAR_PATH_KEY as string;
   const headerKey = process.env?.ORDZAAR_HEADER_KEY as string;
 
-  await Promise.all(
-    uids.map(async (uid) => {
-      const contents = await axios.post(
-        `${endpoint}?x-allow-key=${pathKey}`,
-        {
-          collectionUid: uid,
+  for (const uid of uids) {
+    const contents = await axios.post(
+      `${endpoint}?x-allow-key=${pathKey}`,
+      {
+        collectionUid: uid,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          [`${headerKey}`]: secretKey,
         },
+      },
+    );
+
+    let sha = null;
+    try {
+      const { data } = await octokit.request(
+        "GET /repos/{owner}/{repo}/contents/{path}",
         {
+          owner: "ordzaar",
+          repo: "originals",
+          path: `collections/${uid}/inscription.json`,
           headers: {
-            Authorization: `Bearer ${token}`,
-            [`${headerKey}`]: secretKey,
+            "X-GitHub-Api-Version": "2022-11-28",
           },
         },
       );
+      sha = (data as any).sha ?? null;
+    } catch (e) {
+      console.error(e);
+    }
 
-      let sha = null;
-      try {
-        const { data } = await octokit.request(
-          "GET /repos/{owner}/{repo}/contents/{path}",
-          {
-            owner: "ordzaar",
-            repo: "originals",
-            path: `collections/${uid}/inscription.json`,
-            headers: {
-              "X-GitHub-Api-Version": "2022-11-28",
-            },
-          },
-        );
-        sha = (data as any).sha ?? null;
-      } catch (e) {
-        console.error(e);
-      }
-
-      console.log(`Updating ${uid}...`);
-      await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-        owner: "ordzaar",
-        repo: "originals",
-        path: `collections/${uid}/inscription.json`,
-        message: `chore(bot): update ${uid} hashlist`,
-        content: btoa(JSON.stringify(contents.data.inscriptions)),
-        sha,
-      });
-    }),
-  );
+    console.log(`Updating ${uid}...`);
+    await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+      owner: "ordzaar",
+      repo: "originals",
+      path: `collections/${uid}/inscription.json`,
+      message: `chore(bot): update ${uid} hashlist`,
+      content: btoa(JSON.stringify(contents.data.inscriptions)),
+      sha,
+    });
+  }
 }
 
 main().then(() => console.log("Done!"));
